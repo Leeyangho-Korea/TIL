754180445f9a67bc4091acdaa6271d6ce6555725
henry <tootoo741@naver.com>
2024-02-25 18:46:12 +0900

    Input//Output

---
+++ a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> // --- 헤더 파일
+using namespace std;// std라는 네임스페이스 사용. 
+// 네임스페이스란 : 많은 라이브러리를 불러서 사용할 때 변수명 중복을 방지하기 위해
+// 변수명에 범위를 걸어놓는 것.
+string a; // 문자열 변수 a 선언
+int main(){
+cin >> a;// 변수 a 입력받기.
+cout << a << "\n";// 변수 a 출력하기. 출력함수 : cout, printf
+return 0; // main 함수 종료 코드
+}
