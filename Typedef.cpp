14fa4c4164136826445af0a1671d51328b200ebf
henry <tootoo741@naver.com>
2024-02-25 18:51:38 +0900

    Typedef

---
+++ Typedef.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std; 
+typedef int i; // typedef <타입> <별칭> typedef를 통해 실제 타입이름 대신 별칭으로 사용 가능.
+int main(){
+i a = 1; // int a == i a _typedef로 int를 i로 새로 별칭하고 int를 i로 표현함.
+cout << a << '\n'; 
+return 0;
+}
+/* 1 */
