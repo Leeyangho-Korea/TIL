f30dc3f2f8704ae87aa536b1c9cdd12e30cdaaa3
Leeyangho-Korea <tootoo741@naver.com>
2024-03-02 00:09:24 +0900

    getlin

---
+++ getlin.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+getline(cin, s); //getline은 개행문자('\n')를 끝으로 인식하고 개행 문자 전까지의 입력을 받는다.
+    cout << s << '\n';
+    return 0;
+}
+/*
+ 입력 : 가나 다라
+ 마바사아
+ 
+ 출력 : 가나 다라
+ */
